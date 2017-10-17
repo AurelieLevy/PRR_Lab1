e580662a8fc8ead1c130909e380d66732b00874d
Aurelie Levy <aurelie.levy@heig-vd.ch>
2017-10-17 13:29:10 +0200

    temps attente

--- SyncroHeure/src/syncrohour/MessageManager.java
+++ SyncroHeure/src/syncrohour/MessageManager.java
@@ -24,19 +24,22 @@ public class MessageManager implements Runnable {
    private Date timeSlave;*/
    private final boolean running;
    private long delayMilliSec;
-   private int min = 4;
-   private int max = 60;
-   private int k = 2;
+   private int min;
+   private int max;
+   private int k;
 
-   public MessageManager(int port, String name) throws SocketException {
+   public MessageManager(int port, String name, int min, int max, int k) throws SocketException {
       this.PORT = port;
       this.NAME_MASTER = name;
       socket = new DatagramSocket(port);
       //timeMaster = new Date();
-      running = true;
+      this.running = true;
+      this.min = min;
+      this.max = max;
+      this.k = k;
    }
-   
-   public long getDelay(){
+
+   public long getDelay() {
       return delayMilliSec;
    }
 
@@ -45,10 +48,9 @@ public class MessageManager implements Runnable {
       Date d = new Date(m);
       return new SimpleDateFormat("hh:mm:ss").parse(d);
    }*/
-   /*public void setTimeMaster(Date newTime) {
+ /*public void setTimeMaster(Date newTime) {
       timeMaster = newTime;
    }*/
-
    public int getPORT() {
       return PORT;
    }
@@ -57,63 +59,63 @@ public class MessageManager implements Runnable {
       return NAME_MASTER;
    }
 
-  /* public Date getTimeMaster() {
+   /* public Date getTimeMaster() {
       return timeMaster;
    }*/
-
    //delay
    //voir https://www.developpez.net/forums/d510150/java/general-java/apis/java-util/difference-entre-heures/
    /*private double calculDelay(Date timeMaster, Date timeSlave) {
       return (((timeMaster.getTime() - timeSlave.getTime()) / 1000) / 2);
    }*/
-
    /**
-    * Permet de gérer l'envoi/reception des delay_XXXX
-    * Format:
-    * delay_request: tableau de byte [nomMsg, id]
-    * delay_response: tableau de byte [nomMsg, id, h]
+    * Permet de gérer l'envoi/reception des delay_XXXX Format: delay_request:
+    * tableau de byte [nomMsg, id] delay_response: tableau de byte [nomMsg, id,
+    * h]
     */
    @Override
    public void run() {
       byte id = 0x00;
       byte DELAY_REQUEST = 0x03,
               DELAY_RESPONSE = 0x04;
-      
+
       //date de l'envoi de la requête. Il s'agit du nb de ms depuis 01.01.1970
       long dateSendRequest;
       long dateReceiveRequest;
-      
-      
+
       while (running) {
          try {
-            //String message = "Hello from slave";                                    
-            //byte[] buffer = message.getBytes();
-            id++;
-            byte[] buffer = new byte[]{DELAY_REQUEST, id};
+            try {
+               //String message = "Hello from slave";
+               //byte[] buffer = message.getBytes();
+               id++;
+               byte[] buffer = new byte[]{DELAY_REQUEST, id};
 
-            InetAddress address = InetAddress.getByName(NAME_MASTER);
-            DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, PORT);
-            dateSendRequest = System.currentTimeMillis();
-            socket.send(packet);//send DELAY_REQUEST
+               InetAddress address = InetAddress.getByName(NAME_MASTER);
+               DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, PORT);
+               dateSendRequest = System.currentTimeMillis();
+               socket.send(packet);//send DELAY_REQUEST
 
-            buffer = new byte[256];
-            packet = new DatagramPacket(buffer, buffer.length);
-            socket.receive(packet);
-            //verification that we received DELAY_RESPONSE => name = 0x04
-            if (packet.getData()[0] == DELAY_RESPONSE && packet.getData()[1] == id) {
-               dateReceiveRequest = packet.getData()[2];
-               //calcul of the delay
-                delayMilliSec = (dateReceiveRequest - dateSendRequest)/2;    
+               buffer = new byte[256];
+               packet = new DatagramPacket(buffer, buffer.length);
+               socket.receive(packet);
+               //verification that we received DELAY_RESPONSE => name = 0x04
+               if (packet.getData()[0] == DELAY_RESPONSE && packet.getData()[1] == id) {
+                  dateReceiveRequest = packet.getData()[2];
+                  //calcul of the delay
+                  delayMilliSec = (dateReceiveRequest - dateSendRequest) / 2;
+               }
+               //String messageRecieved = new String(packet.getData());
+            } catch (UnknownHostException ex) {
+               Logger.getLogger(MessageManager.class.getName()).log(Level.SEVERE, null, ex);
+
+            } catch (IOException ex) {
+               Logger.getLogger(MessageManager.class.getName()).log(Level.SEVERE, null, ex);
             }
-            //String messageRecieved = new String(packet.getData());
-         } catch (UnknownHostException ex) {
+            Thread.sleep((min + (int) (Math.random() * ((max - min) + 1))));
+         } catch (InterruptedException ex) {
             Logger.getLogger(MessageManager.class.getName()).log(Level.SEVERE, null, ex);
 
-         } catch (IOException ex) {
-            Logger.getLogger(MessageManager.class.getName()).log(Level.SEVERE, null, ex);
          }
-         //TODO
-         //Thread.sleep(PORT);
       }
       socket.close();
    }
--- SyncroHeure/src/syncrohour/SyncroHour_Slave.java
+++ SyncroHeure/src/syncrohour/SyncroHour_Slave.java
@@ -20,12 +20,12 @@ public class SyncroHour_Slave {
 
    private final String nameSlave;
    private static long timeSlaveMilliSec;
+
    //private int countUnknown = 1;
    /*private byte[] slaveTime;
    private byte[] slaveDifference;
    private byte[] slaveDelay;
    private Date systHour;*/
-
    public SyncroHour_Slave(String name) {
       if (name == null || name == "") {
          System.err.println("slave's name missing");
@@ -34,8 +34,8 @@ public class SyncroHour_Slave {
          nameSlave = name;
       }
    }
-   
-   public long getTimeSlaveMilliSec(){
+
+   public long getTimeSlaveMilliSec() {
       return timeSlaveMilliSec;
    }
 
@@ -43,15 +43,26 @@ public class SyncroHour_Slave {
     * @param args the command line arguments
     */
    public static void main(String[] args) {
-
-      multicastManager multiM = new multicastManager(2223, "multicast1", "239.10.10.1");
-      multiM.run();
+      //value for the waiting time
+      int min = 4;
+      int max = 60;
+      int k = 2;
       
-      while(!multiM.getIsDoneOnce());
+      multicastManager multiM = new multicastManager(2223, "multicast1", "239.10.10.1");
+
+      multiM.run();
+
+      while (!multiM.getIsDoneOnce());
+      try {
+         Thread.sleep((min + (int)(Math.random() * ((max - min) + 1))));
+      } catch (InterruptedException ex) {
+         Logger.getLogger(SyncroHour_Slave.class.getName()).log(Level.SEVERE, null, ex);
+      }
 
       MessageManager msgM;
+
       try {
-         msgM = new MessageManager(2222, "nadir-PC");
+         msgM = new MessageManager(2222, "nadir-PC",min, max, k);
          msgM.run();
          long shift = multiM.getGap() + msgM.getDelay();
          timeSlaveMilliSec = System.currentTimeMillis() + shift;//change current time of slave
