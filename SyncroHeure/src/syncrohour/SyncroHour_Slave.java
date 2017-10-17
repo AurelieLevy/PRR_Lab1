3725a9ec0bc94f48d024d9fda33e44cd796fb970
Aurelie Levy <aurelie.levy@heig-vd.ch>
2017-10-17 15:28:28 +0200

    thread

--- SyncroHeure/src/syncrohour/SyncroHour_Slave.java
+++ SyncroHeure/src/syncrohour/SyncroHour_Slave.java
@@ -8,7 +8,7 @@ package syncrohour;
 import java.net.SocketException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Timer;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -20,6 +20,7 @@ public class SyncroHour_Slave {
 
    private final String nameSlave;
    private static long timeSlaveMilliSec;
+   private static boolean calculation;
 
    //private int countUnknown = 1;
    /*private byte[] slaveTime;
@@ -32,11 +33,16 @@ public class SyncroHour_Slave {
          throw new Error("invalid Parameter");
       } else {
          nameSlave = name;
+         calculation = true;
       }
    }
 
    public long getTimeSlaveMilliSec() {
       return timeSlaveMilliSec;
+   }
+   
+   public void setCalculation(boolean b){
+      calculation = b;
    }
 
    /**
@@ -49,32 +55,37 @@ public class SyncroHour_Slave {
       int k = 2;
 
       multicastManager multiM = new multicastManager(2223, "multicast1", "239.10.10.1");
+      Thread threadMulticast = new Thread(multiM);
 
-      multiM.run();
+      //multiM.run();
+      threadMulticast.start();
 
       while (!multiM.getIsDoneOnce());
 
       try {
-         Thread.sleep((min + (int) (Math.random() * ((max - min) + 1))));
+         TimeUnit.SECONDS.sleep((min + (int) (Math.random() * ((max - min) + 1))));
       } catch (InterruptedException ex) {
          Logger.getLogger(SyncroHour_Slave.class.getName()).log(Level.SEVERE, null, ex);
       }
 
       MessageManager msgM;
-
       try {
          msgM = new MessageManager(2222, "NADIR-PC", min, max, k);
-         msgM.run();
-         while (true) {
+         Thread threadPtToPt = new Thread(msgM);
+         while (calculation) {
             long shift = multiM.getGap() + msgM.getDelay();
             timeSlaveMilliSec = System.currentTimeMillis() + shift;//change current time of slave
             System.out.println(new SimpleDateFormat("dd MM yyyy HH:mm:ss").format(new Date(timeSlaveMilliSec)));
          }
-
       } catch (SocketException ex) {
          Logger.getLogger(SyncroHour_Slave.class.getName()).log(Level.SEVERE, null, ex);
       }
 
+      //try {
+      // msgM.run();
+      /*} catch (SocketException ex) {
+         Logger.getLogger(SyncroHour_Slave.class.getName()).log(Level.SEVERE, null, ex);
+      }*/
    }
 
 }
