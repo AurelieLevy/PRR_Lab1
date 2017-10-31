27044156d377eee63444abb82cd4c0267ca92a67
Aurelie Levy <aurelie.levy@heig-vd.ch>
2017-10-31 19:14:17 +0100

    ajout d'un commentaire sur la division en 2 projet

--- SyncroHeure/src/syncrohour/SyncroHour_Slave.java
+++ SyncroHeure/src/syncrohour/SyncroHour_Slave.java
@@ -5,6 +5,16 @@
  * But: Gestion principale de l'esclave
  * Lance le thread de multicast, lequel gerera le lancement du thread du 
  * point a point
+ * 
+ * Commentaires: Nous avons separe le laboratoire en deux parties afin d'avoir
+ * un maitre et un esclave bien distincts. Cependant, dans les deux programmes,
+ * vous trouverez un fichier Utils.java. Il s'agit des constantes en commun pour
+ * les deux. Pour le bon fonctionnement du protocole entier, il faut s'assurer 
+ * que les deux fichiers correspondent.
+ * Cela permet, dans le cas d'un changement de maitre par exemple, de pouvoir
+ * modifier uniquement ce qui est necessaire sans devoir obligatoirement toucher
+ * au code.
+ * 
  */
 package syncrohour;
 
